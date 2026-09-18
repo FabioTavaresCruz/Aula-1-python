a71f5784b7a3465a677ff203c30a23ad4a0b3d36
FABIO DE OLIVEIRA TAVARES CRUZ <26210911@dsn127m1242267.cfp127aluno.local>
2026-09-18 14:17:53 -0300

    aula1

---
+++ 02.py
@@ -0,0 +1,18 @@
+# Crie um algoritm que leia 3 valores (lados de um triângulo)
+# Determine se formam um triângulo
+# se é equilátero, isósceles ou escaleno.
+
+lado1 = int (input("Digite o valor do primeiro lado: "))
+lado2 = int (input("Digite o valor do segundo lado: "))
+lado3 = int (input("Digite o valor do terceiro lado: "))
+
+if lado1 + lado2 > lado3 and lado1 + lado3 > lado2 and lado2 + lado3 > lado1:
+    if lado1 == lado2 == lado3:
+        print("O triângulo é equilátero.")
+    elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+        print("O triângulo é isósceles.")
+    else:
+        print("O triângulo é escaleno.")
+else:
+    print("Os valores não formam um triângulo.")
+
